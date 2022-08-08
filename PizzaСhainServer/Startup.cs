2ba73313a6ad93cf2115e318912ddaa0a806fd8c
Эдуард Гетман <getman.edik2016@yandex.ru>
2022-08-08 22:22:57 +0300

    Добавьте файлы проекта.

---
+++ PizzaСhainServer/Startup.cs
@@ -0,0 +1,22 @@
+﻿namespace PizzaСhainServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseRouting();
+            app.UseEndpoints(endpoints => endpoints.MapGet("/", async x =>
+            {
+                await x.Response.WriteAsync("<h1>I am alive</h1>");
+            }));
+        }
+    }
+}
